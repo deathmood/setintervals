ce0564b41ed85e7365e8a3e061310f0493c6d7d9
Splitty <splittydev@gmail.com>
2016-09-07 22:29:10 +0200

    Update setintervals.js
    This should save a few bytes

--- setintervals.js
+++ setintervals.js
@@ -1,8 +1,7 @@
 window.setIntervals = function (callback, period, n) {
   var i = 0, interval = setInterval(function () {
-    if (i < n) {
+    if (i++ < n) {
       callback()
-      i++
     } else {
       clearInterval(interval)
     }
